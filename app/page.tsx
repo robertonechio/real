2250868feed07db5ca045d208828f6848b62405c
Roberto Nechio <125361131+robertonechio@users.noreply.github.com>
2023-09-21 20:17:54 +0000

    Parte 4

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,15 @@ export default function Home() {
     <>
       <div className='bg-zinc-950 w-screen'>
       <div className='flex align-center justify-center flex-wrap mx-6'>
-      <Formula name={'Área e perimetro de triângulos e muitas outras coisas.'} type={0} />
-      <Formula name={'Área e perímetro de quadriláteros'} type={1} />
-      <Formula name={'Área e perimetro de triângulos'} type={2} />
-      <Formula name={'Área e perímetro de quadriláteros'} type={3} />
+      <Formula name={'Área e perimetro de triângulos'} type={0} />
+      <Formula name={'Porcentagem'} type={1} />
+      <Formula name={'Média'} type={1} />
+      <Formula name={'Proporção'} type={1} />
+      <Formula name={'Razão'} type={1} />
+      <Formula name={'Poligonos'} type={2} />
+      <Formula name={'Poliedros'} type={2} />
+      <Formula name={'Juros compostos'} type={3} />
+      <Formula name={'Juros simples'} type={3} />
       </div>
       {/* <div className='flex items-center w-screen h-screen bg-zinc-900 '>
       <Image src={Green} alt={''} className='hover:opacity-20'/>
